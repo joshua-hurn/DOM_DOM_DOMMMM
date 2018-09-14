35b79e0bec130c2ce01ca5a7acad2ce009d0684e
joshua-hurn <joshua.hurn@hotmail.com>
2018-09-14 17:53:35 -0500

    end of class commit

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let BoxButton = document.createElement('button')
-    button.innerHTML = "Add Square"
-
-
+    let boxButton = document.createElement('button');
+    let btnText = document.createTextNode("Add Square");
+    boxButton.appendChild(btnText);
+    document.body.appendChild(boxButton);
     
+    function divMaker() {
+        let newDiv = document.createElement('div');
+        newDiv.setAttribute("class", "boxes");
+        newDiv.setAttribute("id", "boxDiv1");
+        document.body.appendChild(newDiv);
+    }
+    
+    boxButton.onclick = divMaker;
 
-//
+    if (document.newDiv.id = "boxDiv1") {
+        document.newDiv.id = ("boxDiv" + 1);
+    }
 });
